256b03635d9c0e70286245588bf41dcc69efdf46
Md Yusuf Ansari <100552609+iamyusuf01@users.noreply.github.com>
2022-10-24 07:11:11 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int a[10],i,sum=0;
+  printf("Enter 10 number\n");
+  for(i=0;i<=9;i++)
+    
+  scanf("%d",&a[i]);
+  
+  for(i=0;i<=9;i++)
+    sum=sum+a[i];
+    printf("%d",sum);
+
+
+  
+}
